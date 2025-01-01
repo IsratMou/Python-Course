13fe92fb9a34bb465c1c31b6cfa0372bb074dd85
Israt Jahan Mou <139980282+IsratMou@users.noreply.github.com>
2025-01-01 21:08:32 +0600

    Add files via upload

---
+++ _06Input.py
@@ -0,0 +1,7 @@
+a= int(input("enter number 1 :"))
+b= int(input("enter number 2 :"))
+
+print("Number a is : ", a)
+print("Number b is : ", b)  #duplicate niche copy korar easy system holo (alt+shift+down arrow)
+
+print("Sum is ", a + b)
