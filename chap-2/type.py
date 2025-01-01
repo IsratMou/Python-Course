e037ba820dd71c823bc32d734d4deb9a89ca9f04
Israt Jahan Mou <139980282+IsratMou@users.noreply.github.com>
2025-01-01 20:19:20 +0600

    Add files via upload

---
+++ chap-2/type.py
@@ -0,0 +1,5 @@
+a = "31.2"
+b = float(a) # a but the type should be float.. typrcast kore dilo  from string to float
+t = type(b) 
+
+print(t)
---
+++ chap-2/variables.py
@@ -0,0 +1,18 @@
+a=1
+b=2          #int type
+c= "Harry"  #string type
+d=72.13      #float type
+
+print(a+b)
+
+a = 1 # a is an integer
+
+b = 5.22 # b is a floating point number
+
+c = "Harry" # c is a string
+
+d = False # d is a boolean variable
+
+e = None # e is a none type variable. ekhane nothing k mark korar jonno none type use kora hoy
+
+
