5294a8261ed9eeb4cd3f16005ef3f12780c3e393
Israt Jahan Mou <139980282+IsratMou@users.noreply.github.com>
2025-01-14 05:24:39 +0600

    Update _06Input.py

--- chap-2/_06Input.py
+++ chap-2/_06Input.py
@@ -5,3 +5,13 @@ print("Number a is : ", a)
 print("Number b is : ", b)  #duplicate niche copy korar easy system holo (alt+shift+down arrow)
 
 print("Sum is ", a + b)
+
+# Single variable
+s = "Bob"
+print(s)
+
+# Multiple Variables
+s = "Alice"
+age = 25
+city = "New York"
+print(s, age, city)
