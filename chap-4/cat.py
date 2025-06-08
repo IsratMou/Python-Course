8da8bad13a064b4eb23edc9df6c46677d5f46f44
Israt Jahan Mou <139980282+IsratMou@users.noreply.github.com>
2025-06-09 05:29:14 +0600

    Add files via upload

---
+++ chap-4/cat.py
@@ -0,0 +1,35 @@
+print("""
+  ₍ᐢ⑅• ̫•⑅ᐢ₎ദ⸒⸒
+      
+      ଘ(੭˃ᴗ˂)੭
+
+      ʕ•ᴥ•ʔﾉ♡
+
+    ₍˶ •. •⑅₎ა♡
+      
+      """)
+
+# Option 1: A simple cute ASCII cat
+ascii_cat = r"""
+ /\_/\  
+( o.o ) 
+ > ^ <
+"""
+print(ascii_cat)
+
+print(r"""
+       /^\
+      |   |
+/\     |_|     /\
+| \___/' `\___/ |
+ \_/  \___/  \_/
+  |\__/   \__/|
+  |/  \___/  \|
+ ./\__/   \__/\,
+ | /  \___/  \ |
+ \/     V     \/
+      
+""")
+
+a = "              Hello,          World!          "
+print(a.strip()) # returns "Hello, World!"
---
+++ chap-4/prc.py
@@ -0,0 +1,8 @@
+l = float(input("Enter the length of the rectangle: "))
+w = float(input("Enter the width of the rectangle: "))
+
+area = l*w
+
+print(f"The area of a rectangle is : {area} cm²")
+# numlock+ alt + 0178 for cm² symbol
+
