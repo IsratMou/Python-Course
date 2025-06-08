9ee9d1ff73a44a18350894346dfd5d03b4f61d74
Israt Jahan Mou <139980282+IsratMou@users.noreply.github.com>
2025-06-09 05:49:14 +0600

    Add files via upload
    matlib game

---
+++ chap-5/inputExercise.py
@@ -0,0 +1,18 @@
+# Mad Libs Game
+# This program will ask the user for various inputs and then print a story using those inputs.
+# The user will be prompted for inputs for each part of the story.
+
+# ----------------------- Mad Libs Game------------------------
+
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
+verb1 = input("Enter a verb ending in -ing: ")
+adjective3 = input("Enter another adjective: ")
+
+print("--------Welcome to the Mad Libs game!---------")
+print(f"today i went to a {adjective1} zoo. ")
+print(f"In an exhibit, i saw a {noun1} .")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"i was {adjective3}!")
